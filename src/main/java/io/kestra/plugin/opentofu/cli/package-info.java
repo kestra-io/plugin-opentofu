d37c2de625cdfc2177863e32ea0a15b7811729be
Malay Dewangan <malaydewangan310@gmail.com>
2026-03-23 21:59:34 +0530

    feat: add CLI task

---
+++ src/main/java/io/kestra/plugin/opentofu/cli/package-info.java
@@ -0,0 +1,9 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using OpenTofu.",
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
+)
+package io.kestra.plugin.opentofu.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
